a94013698c78a253b99a1b5aeb0f9dfe59e85bfe
GeorgiAndreev <georgi_andreev@yahoo.com>
2016-07-13 14:27:09 +0300

    added constants in Employee class:  WORKING_HOURS_IN_A_WORKING_DAY and MULTIPLY_COEF_FOR_OVERTIME_WORKING_HOUR

--- Project_EightHomework_GeorgiAndreev/src/eightHomework_GeorgiAndreev/Employee.java
+++ Project_EightHomework_GeorgiAndreev/src/eightHomework_GeorgiAndreev/Employee.java
@@ -2,6 +2,8 @@ package eightHomework_GeorgiAndreev;
 
 public class Employee extends Person{
 	
+	private static final float MULTIPLY_COEF_FOR_OVERTIME_WORKING_HOUR = 1.5f;
+	private static final int WORKING_HOURS_IN_A_WORKING_DAY = 8;
 	private float daySalary;
 
 	public Employee(String name, short age, Gender gender, float daySalary) {
@@ -21,12 +23,12 @@ public class Employee extends Person{
 	}
 	
 	float calculateOvertime(float hours) {
-		if (hours < 0) {
+		if (hours <= 0) {
 			System.out.println("Invalid overtime hours.");
 			return 0;
 		}
-		float moneyForHour = this.daySalary / 8;
-		float moneyForOvertimeHour = moneyForHour * 1.5f;
+		float moneyForHour = this.daySalary / WORKING_HOURS_IN_A_WORKING_DAY;
+		float moneyForOvertimeHour = moneyForHour * MULTIPLY_COEF_FOR_OVERTIME_WORKING_HOUR;
 		return moneyForOvertimeHour * hours;
 	}
 }
